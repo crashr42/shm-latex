f05fabc83cb7b9fc7a7f4c6cae348bb8323bc14b
nikita <nikita.kem@gmail.com>
2013-05-13 00:16:21 +0700

    adding picturies

--- images/convert.rb
+++ images/convert.rb
@@ -3,6 +3,6 @@
 Dir.entries('.').each do |entry|
     next if !File.file?(entry) || File.extname(entry) != '.png'
     puts "Converting #{File.basename(entry, '.*')} ..."
-    `convert -colorspace Gray -flatten #{entry} #{entry}`
+    `convert -flatten #{entry} #{entry}`
     `./png2eps #{entry} > #{File.basename(entry, '.*')}.eps`
 end
