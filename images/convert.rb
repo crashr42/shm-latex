37f4af65a65ab9299c92cb553429c59a13aab1f6
nikita <nikita.kem@gmail.com>
2013-05-12 23:06:01 +0700

    some fixes

--- images/convert.rb
+++ images/convert.rb
@@ -3,6 +3,6 @@
 Dir.entries('.').each do |entry|
     next if !File.file?(entry) || File.extname(entry) != '.png'
     puts "Converting #{File.basename(entry, '.*')} ..."
-    `convert -flatten #{entry} #{entry}`
+    `convert -colorspace Gray -flatten #{entry} #{entry}`
     `./png2eps #{entry} > #{File.basename(entry, '.*')}.eps`
 end
