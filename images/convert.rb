fa3527e732c61929c2799f88eac2ed1a99ec8d60
nikita <nikita.kem@gmail.com>
2013-05-19 23:35:54 +0700

    adding to be applications, move images as is to applications, update title page

--- images/convert.rb
+++ images/convert.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/ruby
 
+puts "Clean files"
+`rm -rf *.new.png`
+`rm -rf *.eps`
+`rm -rf *.pdf`
+
 Dir.entries('.').each do |entry|
     next if !File.file?(entry) || File.extname(entry) != '.png'
     base_name = File.basename(entry, '.*')
